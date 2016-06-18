30ccac83c5d38516428fc5360634893d314b3882
prashannth <vprashannth@ymail.com>
2016-06-18 13:01:06 +0530

    fontawesome update

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://prashannth.herokuapp.com',
+        url: 'http://prashannth.com',
         mail: {
             transport: 'SMTP',
                 options: {
